ee59a9f929b7070b5d739e976dcee46c3a602811
a4z <harald.achitz@gmail.com>
2022-09-07 21:35:39 +0200

    Change let to  var to see if it  works on older swift version

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,8 +1,8 @@
 import CppLib
 
-let myPoint = MyPoint(3,4)
+var myPoint = MyPoint(3,4)
 print( myPoint.getx(), myPoint.gety() )
 
-let movedPoint = moveXY(myPoint, 2,3)
+var movedPoint = moveXY(myPoint, 2,3)
 print( movedPoint.getx(), movedPoint.gety() )
 
